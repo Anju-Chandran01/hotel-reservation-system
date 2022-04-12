a3ba310b1f382a65437962c4b817803dc81f2e95
Anju-Chandran01 <anjuchandrancs01@gmail.com>
2022-04-12 02:24:20 -0700

    Added : find hotel according to the cheap rate

---
+++ src/test/java/com/blz/hotelreservationsystem/HotelReservationTest.java
@@ -0,0 +1,11 @@
+package com.blz.hotelreservationsystem;
+
+import org.junit.jupiter.api.Test;
+
+public class HotelReservationTest {
+
+    @Test
+    void toFindTheCheapestHotel() {
+        HotelReservation.findCheapestHotel();
+    }
+}
